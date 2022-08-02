30520bb6a534186f27f84193f5f549c16ab2b87d
saylinadkar <101880800+saylinadkar@users.noreply.github.com>
2022-08-02 13:27:34 +0530

    firstcommit

---
+++ DAY1_Git and Github/src/com/gapgemini/github/EclipseWithJavaConnection.java
@@ -0,0 +1,11 @@
+package com.gapgemini.github;
+//demo for to connect eclipse with github 
+public class EclipseWithJavaConnection {
+
+	public static void main(String[] args) {
+   System.out.println("welcom to Capgemini C2TC program");
+   
+
+	}
+
+}
