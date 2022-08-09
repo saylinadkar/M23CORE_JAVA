5f417a87517ccf63c35c1f533d875d5c9811fbf6
saylinadkar <101880800+saylinadkar@users.noreply.github.com>
2022-08-09 22:53:02 +0530

    second commit

---
+++ DAY1_Git and Github/src/com/gapgemini/github/Hellojava.java
@@ -0,0 +1,11 @@
+package com.gapgemini.github;
+
+public class Hellojava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello");
+
+	}
+
+}
