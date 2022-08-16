772299e906be217ad2b3cc6772a739e9463cf632
saylinadkar <101880800+saylinadkar@users.noreply.github.com>
2022-08-16 23:46:15 +0530

    second commit

--- Assignment_1_operatorloopingStatement/src/com/cg/operator/UranaryOperator.java
+++ Assignment_1_operatorloopingStatement/src/com/cg/operator/UranaryOperator.java
@@ -5,7 +5,8 @@ public class UranaryOperator {
 	public static void main(String[] args) {
 	 int a=5,b=3;
 	 int x=a++; //5
-	 int y=++b;  //4
+	 @SuppressWarnings("unused")
+	int y=++b;  //4
 	 int res=a+x; //6+5=11
 	 System.out.println(res);
 	 
@@ -14,7 +15,8 @@ public class UranaryOperator {
 	  
 	 int p=5,q=3;
 	 int m=p--; //5
-	 int n=--q; //2
+	 @SuppressWarnings("unused")
+	int n=--q; //2
 	 int res1=p+m; //4+5=9
 	 System.out.println(res1);
 	
