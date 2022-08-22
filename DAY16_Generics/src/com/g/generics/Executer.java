897d86ab37261a624b24ebcab2f3d0c6ef4a0497
saylinadkar <101880800+saylinadkar@users.noreply.github.com>
2022-08-22 15:44:16 +0530

    first commit

---
+++ DAY16_Generics/src/com/g/generics/Executer.java
@@ -0,0 +1,28 @@
+package com.g.generics;
+
+public class Executer {
+
+	public static void main(String[] args) {
+		Sender<String> obj=new Sender<>();
+		obj.setMessege("Generic class demo");
+		obj.sendmessege();
+		System.out.println();
+		
+		// create an email object to be send using sender 
+		Email em=new Email();
+		em.setFrom("sayalinadkar532@gmail.com");
+		em.setTo("sayalinadkar532@gmail.com");
+		em.setSubject("full stack");
+	   em.setBody("About training process");
+	   System.out.println(em.getBody());
+	   
+	   //create sender to send email 
+	   Sender<Email>obj1=new Sender<Email>();
+	  obj1.setMessege(em);
+	
+	  obj1.sendmessege();
+	 
+	 
+	}
+
+}
---
+++ DAY16_Generics/src/com/g/generics/GenericsAdvantages.java
@@ -0,0 +1,10 @@
+package com.g.generics;
+
+public class GenericsAdvantages {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
