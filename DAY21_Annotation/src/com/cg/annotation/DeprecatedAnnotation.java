a848ecf3fbbce3d974a4adea45527727f2608692
saylinadkar <101880800+saylinadkar@users.noreply.github.com>
2022-08-28 23:46:06 +0530

    First commit

---
+++ DAY21_Annotation/src/com/cg/annotation/DeprecatedAnnotation.java
@@ -0,0 +1,21 @@
+package com.cg.annotation;
+
+class A
+{
+	//it will just allows any element no longer to the use.
+	@Deprecated
+	public void print() 
+	{
+		System.out.println("hello guys");
+	}
+}
+public class DeprecatedAnnotation {
+
+	public static void main(String[] args) {
+		A a=new A();
+		a.print();
+		
+
+	}
+
+}
